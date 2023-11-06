a7deaef7ab6c13d839b13bb078a3003948a63391
matous <matous.trca@gmail.com>
2023-11-06 08:11:41 +0100

    espanso global shortcut, improve goodday

--- scripts/goodday.sh
+++ scripts/goodday.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# Load text formatting.
+source $HOME/scripts/text.sh
+
 GD_HEADER="gd-api-token: $GOODDAY_TOKEN"
 GD_USER="$GOODDAY_USER"
 GD_URL="https://api.goodday.work/2.0"
@@ -8,11 +12,30 @@ function getId() {
 }
 
 function fetchTasksWithProject() {
-  local tasks=$( curl -s -H "$GD_HEADER" $GD_URL/user/$GD_USER/action-required-tasks )
-  local projects=$( curl -s -H "$GD_HEADER" $GD_URL/projects )
-  jq -n --argjson tasks "$tasks" --argjson projects "$projects" \
-    '$tasks[] as $t | ($projects[] | select(.id == $t.projectId).name) as $p | "[\($t.status.name)] [\($p)] \($t.name) (\($t.id))"'
+    local tasks=$( curl -s -H "$GD_HEADER" $GD_URL/user/$GD_USER/action-required-tasks )
+    local projects=$( curl -s -H "$GD_HEADER" $GD_URL/projects )
+    jq --argjson tasks "$tasks" --argjson projects "$projects" -n \
+      '
+      [
+        ($tasks[] as $t  | select($t.status.name as $status_name | ["On hold", "Completed", "Dlouhodobý"] | index($status_name) | not) |
+        $projects[] as $p | select($p.id == $t.projectId) | {task_id: $t.id, task_name: $t.name, status_name: $t.status.name, project_name: $p.name, date: $t.recentActivityMoment})
+      ]
+      | sort_by(.date) | reverse
+      | .[] | "[\(.date[8:10]).\(.date[5:7]) \(.date[11:16])] [\(.project_name)] \(.task_name) (\(.task_id))"
+      '
 }
+
+function fetchMessagesWithUser() {
+    local messages=$( curl -s -H "$GD_HEADER" $GD_URL/task/$1/messages )
+    local users=$( curl -s -H "$GD_HEADER" $GD_URL/users )
+    jq --argjson messages "$messages" --argjson users "$users" -n \
+      '
+      [
+        ($messages[] as $m | $users[] as $u | select($u.id == $m.fromUserId) | {text: $m.message, date: "\($m.dateCreated[8:10]).\($m.dateCreated[5:7]) \($m.dateCreated[11:16])", user: $u.name})
+      ]
+      '
+}
+
 
 task=$(fetchTasksWithProject | fzf --layout=reverse)
 taskId=$(getId "$task")
@@ -26,39 +49,25 @@ user_selection=$(printf '%s\n' "${options[@]}" | fzf --layout=reverse)
 case $user_selection in
     "Open")
         echo "Opening the browser with  $taskId..."
-        url="https://www.goodday.work/t/$taskId"
-        if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-        # Linux
-        xdg-open "$url"
-	    elif [[ "$OSTYPE" == "darwin"* ]]; then
-	        # Mac OSX
-	        open "$url"
-	    else
-	        echo "Your OS is not supported for this operation."
-	    fi
+        xdg-open "https://www.goodday.work/t/$taskId"
         ;;
 	"Detail")
         echo "Opening details..."
-        echo $task
-        echo "Project: $project_id"
-        echo "==========================="
-        messages=$( curl -s -H "$GD_HEADER" $GD_URL/task/$taskId/messages )
+        echo $task | fold -w 64
+        echo "================================================================"
+        messages=$(fetchMessagesWithUser "$taskId")
         length=$(echo "$messages" | jq '. | length')
         for (( i=0; i<$length; i++ ))
-        do
-          message=$(echo "$messages" | jq -r ".[$i].message")
-          date_created=$(echo "$messages" | jq -r ".[$i].dateCreated")
-          user_id=$(echo "$messages" | jq -r ".[$i].fromUserId")
-            if [[ ! -z "$message" && "$message" != "null" ]]; then
-              if [[ "$user_id" == "$GD_USER" ]]; then
-                echo "User: Me"
-          else
-            echo "User: $user_id"
+          do
+            text=$(echo "$messages" | jq -r ".[$i].text")
+            date=$(echo "$messages" | jq -r ".[$i].date")
+            user=$(echo "$messages" | jq -r ".[$i].user")
+            if [[ ! -z "$text" && "$text" != "null" ]]; then
+              echo -e "${BOLD}$user${CLEAR}"
+              echo -e "${UNDERLINE}$date${CLEAR}"
+              echo -e "$text" | fold -w 64
+              echo "================================================================"
             fi
-              echo "Date Created: $date_created"
-              echo "$message"
-              echo "==========================="
-          fi
         done
        ;;
     "Reply")
@@ -71,7 +80,8 @@ case $user_selection in
     	echo "Change AR:"
     	users=$( curl -s -H "$GD_HEADER" $GD_URL/users )
 		  ar_user=$(echo $users | jq -r '.[] | .name + " (" + .id + ")"' | fzf --layout=reverse)
-		  ar_user_id=$(getId "$user")
+		  ar_user_id=$(getId "$ar_user")
+		  echo "Selected user: $ar_user_id"
       echo "Your reply text:"
       read user_reply
       echo "Time report:"
@@ -83,6 +93,8 @@ case $user_selection in
       curl -s -X POST --location "${GD_URL}/task/${taskId}/reply" -H "$GD_HEADER" -H "Content-Type: application/json"  -d '{"userId": "'"$GD_USER"'", "actionRequiredUserId": "'"$ar_user_id"'", "message": "'"$user_reply"'"}'
       echo "Time log response:"
       curl -s -X POST --location "${GD_URL}/task/${taskId}/time-report" -H "$GD_HEADER" -H "Content-Type: application/json"  -d '{"userId": "'"$GD_USER"'", "reportedMinutes": "'"$reported_minutes"'"}'
+      echo "Check the status in a browser"
+      xdg-open "https://www.goodday.work/t/$taskId"
       ;;
     *)
       echo "Invalid option: $user_selection"
