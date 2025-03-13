2ac4493925c0edbd92feda41a007766e752aa37f
matous <matous.trca@gmail.com>
2025-03-13 16:16:19 +0100

    Add keymaps for moving selection and entering command mode

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,14 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = vim.keymap.set
+
+map("n", ";", ":", { desc = "CMD enter command mode" })
+map("i", "jk", "<ESC>")
+-- move selection up / down
+map("v", "<C-j>", ":m '>+1<CR>gv=gv")
+map("v", "<C-k>", ":m '<-2<CR>gv=gv")
+map("n", "<C-j>", ":m .+1<CR>==")
+map("i", "<C-j>", "<Esc>:m .+1<CR>==gi")
+map("n", "<C-k>", ":m .-2<CR>==")
+map("i", "<C-k>", "<Esc>:m .-2<CR>==gi")
