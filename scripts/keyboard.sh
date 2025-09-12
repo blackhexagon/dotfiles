aac4e54c72f18f3a0145866d7bedfd7cc90433de
matous <matous.trca@gmail.com>
2025-09-12 14:01:50 +0200

    Update essential-installs.sh, keyboard.sh, and tmux.sh scripts

--- scripts/keyboard.sh
+++ scripts/keyboard.sh
@@ -4,23 +4,18 @@
 USER_HOME=$(eval echo ~${SUDO_USER})
 CUSTOM_LAYOUT="$USER_HOME/keyboard/layout"
 TARGET_LAYOUT="/usr/share/X11/xkb/symbols/cz"
+
 # Check if running as root
-# if [[ $EUID -ne 0 ]]; then
-#   echo "Please run this script as root (e.g., sudo ./script.sh)"
-#   exit 1
-# fi
+if [[ $EUID -ne 0 ]]; then
+  echo "Please run this script as root (e.g., sudo ./script.sh)"
+  exit 1
+fi
 
 # Append custom layout
 echo "Appending custom layout..."
 cat "$CUSTOM_LAYOUT" >>"$TARGET_LAYOUT"
 
-sudo nano /usr/share/X11/xkb/rules/evdev.xml
-# <variant>
-#   <configItem>
-#     <name>u2b22</name>
-#     <description>Czech (u2b22)</description>
-#   </configItem>
-# </variant>
+sudo $EDITOR /usr/share/X11/xkb/rules/evdev.xml
 
 gsettings set org.gnome.desktop.input-sources xkb-options "['lv3:ralt_switch']"
 
