74799b6d4385ed335de8922e66c4f609af2c747f
matous <matous.trca@gmail.com>
2025-09-12 08:05:39 +0200

    update neovim install, add zoxide

--- scripts/neovim.sh
+++ scripts/neovim.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
-# Install ascii image converter
-echo 'deb [trusted=yes] https://apt.fury.io/ascii-image-converter/ /' | sudo tee /etc/apt/sources.list.d/ascii-image-converter.list
-sudo apt update
-sudo apt install -y ascii-image-converter
-sudo apt install -y neovim
+sudo snap install nvim --classic
