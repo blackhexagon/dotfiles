d4a4ee34db7c7743d05fc650dcbc42b7411b0f91
matous <matous.trca@gmail.com>
2025-12-31 22:09:42 +0100

    Add initial Hypr configuration files

--- scripts/tmux.sh
+++ scripts/tmux.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
-sudo apt install tmux
+
+# Detect OS and install tmux with appropriate package manager
+if [ -f /etc/arch-release ]; then
+    sudo pacman -S --noconfirm tmux
+elif [ -f /etc/debian_version ]; then
+    sudo apt install -y tmux
+else
+    echo "Unsupported OS"
+    exit 1
+fi
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 tmux source ~/.tmux.conf
