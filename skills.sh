c67d0fbc51768e4d48dcf2a57c318a32f1e94bad
matous <matous.trca@gmail.com>
2026-09-02 11:54:58 +0200

    add ssh hetzner + new skilss

--- skills.sh
+++ skills.sh
@@ -2,7 +2,7 @@
 
 #Global
 npx skills add googleworkspace/cli
-npx skills@latest add mattpocock/skills #https://www.aihero.dev/my-grill-me-skill-has-gone-viral
+npx skills@latest add mattpocock/skills #https://www.aihero.dev/my-grill-me-skill-has-gone-viral & https://blog.master.dev/introducing-ai-skills-for-real-engineers/
 
 #Project
 npx skills add nette/claude-code
@@ -10,3 +10,4 @@ npx skills add stolinski/s-stack --skill html
 npx skills add GoogleChrome/modern-web-guidance
 npx skills add https://evilmartians.com/agent-skills --skill tailwind-best-practices
 npx skills add AminBlg/SimpleEnglish
+npx skills add Jakubantalik/transitions.dev # https://transitions.dev/skill.html
